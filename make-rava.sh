42be5eeda54e0555f6ef0d9bf8d6ca384fa44a4b
Luemery <luemery@live.com>
2022-10-04 20:24:22 -0300

    Pastas repetidas retiradas

--- make-rava.sh
+++ make-rava.sh
@@ -7,7 +7,7 @@ else
     mkdir micro-rAVA
     cp rAVA/terminal.py micro-rAVA/
     mkdir micro-rAVA/bot
-    cp -r rAVA/bot micro-rAVA/bot
+    cp -r rAVA/bot micro-rAVA/
     mkdir micro-rAVA/livros
     cp -r rAVA/livros micro-rAVA/livros
     zip -r micro-rAVA micro-rAVA
@@ -18,11 +18,11 @@ else
     mkdir full-rAVA
     cp rAVA/*.py full-rAVA/
     mkdir full-rAVA/bot
-    cp -r rAVA/bot full-rAVA/bot
+    cp -r rAVA/bot full-rAVA/
     mkdir full-rAVA/livros
-    cp -r rAVA/livros full-rAVA/livros
+    cp -r rAVA/livros full-rAVA/
     mkdir full-rAVA/website
-    cp -r rAVA/website full-rAVA/website
+    cp -r rAVA/website full-rAVA/
     zip -r full-rAVA full-rAVA
     rm -fdr full-rAVA/
 fi
